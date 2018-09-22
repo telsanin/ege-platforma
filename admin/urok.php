635033c500972eb59014670aa4e5ee076fb5587f
Tel <dtelegin@mail.ru>
2018-09-22 03:34:47 +0300

    vopr

--- admin/urok.php
+++ admin/urok.php
@@ -84,7 +84,7 @@ while ($row = $res->fetch_assoc()) {
 
 //Вопросы:
 echo "</br><p><b>Вопросы</b>:</p>";
-$SqlQuery = "SELECT DISTINCT `voprosy`.`id-voprosa`, `voprosy`.`text-voprosa`, `aktualno` FROM `uchenik-voprosy`, `voprosy` WHERE `uchenik-voprosy`.`id-voprosa`=`voprosy`.`id-voprosa` AND `voprosy`.`zadanie`='".$iNomerZadaniya."' AND `uchenik-voprosy`.`uchenik`='".$sUchenik."';";
+$SqlQuery = "SELECT DISTINCT `voprosy`.`id-voprosa`, `voprosy`.`text-voprosa`, `aktualno` FROM `uchenik-voprosy`, `voprosy` WHERE `uchenik-voprosy`.`id-voprosa`=`voprosy`.`id-voprosa` AND `voprosy`.`zadanie`='".$iNomerZadaniya."' AND `voprosy`.`predmet`='".$sPredmet."' AND `uchenik-voprosy`.`uchenik`='".$sUchenik."';";
 $res = $mysqli->query($SqlQuery);
 $res->data_seek(0);
 $iNumDZ = 1;
