7d226cf4550a8f4ad42a2c05edc8f0184b2c51c6
Tel <dtelegin@mail.ru>
2018-10-01 02:30:14 +0300

    stat

--- admin/telsanin.php
+++ admin/telsanin.php
@@ -58,7 +58,7 @@ if($res1->data_seek(0)) {
                     echo "Среднее время выполнения: " . gmdate("H:i:s", $iSredVremya) . "</br>";
                     echo "Общее время выполнения: " . gmdate("H:i:s", $iSumVremya) . "</br></br>";
                 } else {
-                    echo "-</br></br>";
+                    echo "-</br>";
                 }
             }
         }
