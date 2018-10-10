033a1cfcca04bda57a58ef62d456caa6ab8a2dc4
Tel <dtelegin@mail.ru>
2018-10-10 15:36:06 +0300

    test2

--- admin-router.php
+++ admin-router.php
@@ -8,6 +8,7 @@
 //работает так: logger('любой текст');
 //include $_SERVER["DOCUMENT_ROOT"]."/PART_logger.php";
 //
+//
 
 //$sParametr1 = explode( '/', $sUrl )[0];
 $sParametr2 = explode( '/', $sUrl )[1];
